3219176ef1ae501054eacb2123d77aafd20d6244
Antonio <antoniotorr5555@gmail.com>
2020-06-15 22:13:57 -0500

    Telefono Propietario Agregado

--- Scripts/s-02-entidades.sql
+++ Scripts/s-02-entidades.sql
@@ -46,6 +46,13 @@ create table propietario(
   email varchar2(150) not null
 );
 
+create table telefono_propietario(
+	telefono_propietario_id number(10,0) constraint telefono_propietario_pk primary key,
+	num_telefono varchar2(15) constraint telefono_propietario_num_telefono_uk unique,
+	propietario_id number(10,0) constraint telefono_propietario_propietario_id
+		references propietario(propietario_id)
+);
+
 create table status_vehiculo(
   status_vehiculo_id number(10,0) constraint status_vehiculo_pk primary key,
   clave varchar2(30) not null,
