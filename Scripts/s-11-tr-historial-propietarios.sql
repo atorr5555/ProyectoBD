41c5fd7504d5c4800e2bf6192257c5bf052721fc
Antonio <antoniotorr5555@gmail.com>
2020-06-17 00:06:37 -0500

    Triggers históricos

--- Scripts/s-11-tr-historial-propietarios.sql
+++ Scripts/s-11-tr-historial-propietarios.sql
@@ -5,37 +5,22 @@
 set serveroutput on
 
 create or replace trigger tr_historial_propietarios
-	before insert or update of propietario_id on vehiculo
-	for each row
-declare
-	v_historico_propietario_id historico_propietario.historico_propietario_id%type;
-
+  after insert or update of propietario_id on vehiculo
+  for each row
 begin
-	case 
-		when updating then
-			-- Si el propietario es el mismo
-			if :old.propietario_id = :new.propietario_id then
-				raise_application_error(-20010, 'El cliente es el mismo');
-			end if;
-
-			-- Obteniendo el registro del histórico para completar el periodo
-			select historico_propietario_id into v_historico_propietario_id
-			from historico_propietario
-			where propietario_id = :old.propietario_id
-			and vehiculo_id = :old.vehiculo_id
-			and inicio_periodo = :old.inicio_periodo;
-
-			-- Se actualiza el registro del historial de propietarios
-			update historico_propietario
-			set fin_periodo = sysdate
-			where historico_propietario_id = v_historico_propietario_id;
-	end case;
-	
-	-- Insertando en historico propietario
-	insert into historico_propietario(historico_propietario_id, inicio_periodo,
-		propietario_id, vehiculo_id)
-	values(seq_historico_propietario.nextval, :new.inicio_periodo,
-		:new.propietario_id, :new.vehiculo_id);
+  case 
+    when updating then
+      -- Si el propietario es el mismo
+      if :old.propietario_id = :new.propietario_id then
+        raise_application_error(-20010, 'El propietario es el mismo');
+      end if;
+  end case;
+  
+  -- Insertando en historico propietario
+  insert into historico_propietario(historico_propietario_id, inicio_periodo,
+    propietario_id, vehiculo_id)
+  values(seq_historico_propietario.nextval, :new.inicio_periodo,
+    :new.propietario_id, :new.vehiculo_id);
 end;
 /
 show errors
---
+++ Scripts/s-11-tr-historico-vehiculo.sql
@@ -0,0 +1,26 @@
+--@Autor: Flores Fuentes Kevin y Torres Verastegui Jose Antonio
+--@Fecha creación: 16/06/2020
+--@Descripción: Trigger para actualizar el histórico del vehículo
+
+set serveroutput on
+
+create or replace trigger tr_historico_vehiculo
+  after insert or update of status_vehiculo_id on vehiculo
+  for each row
+begin
+  case 
+    when updating then
+      -- Si el status es el mismo
+      if :old.status_vehiculo_id = :new.status_vehiculo_id then
+        raise_application_error(-20011, 'El status es el mismo');
+      end if;
+  end;
+
+  -- Insertando en histórico vehículo
+  insert into historico_status_vehiculo(historico_status_vehiculo_id,
+    fecha_status, vehiculo_id, status_vehiculo_id)
+  values(seq_historico_status_vehiculo.nextval, :new.fecha_status,
+    :new.vehiculo_id, :new.status_vehiculo_id);
+end;
+/
+show errors
