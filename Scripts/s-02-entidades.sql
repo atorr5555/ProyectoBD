0b67a6d89ae2180937565b571b4f89bfe5c0de4d
Antonio <antoniotorr5555@gmail.com>
2020-06-16 22:02:59 -0500

    Valor default agregaddo a vehiculo

--- Scripts/s-02-entidades.sql
+++ Scripts/s-02-entidades.sql
@@ -72,8 +72,8 @@ create table vehiculo(
   es_transporte_publico number(1,0) not null,
   es_carga number(1,0) not null,
   es_particular number(1,0) not null,
-  inicio_periodo date not null,
-  fecha_status date default sysdate,
+  inicio_periodo date default sysdate not null,
+  fecha_status date default sysdate not null,
   modelo_id varchar2(10)not null,
   constraint vehiculo_modelo_id_fk foreign key (modelo_id)
     references modelo(modelo_id),
