87445f15c448a88941ce6a502dc98c19b573781a
Antonio <antoniotorr5555@gmail.com>
2020-06-16 22:42:59 -0500

    Trigger Historial Propietario

---
+++ Scripts/s-11-tr-historial-propietarios.sql
@@ -0,0 +1,39 @@
+--@Autor: Flores Fuentes Kevin y Torres Verastegui Jose Antonio
+--@Fecha creación: 16/06/2020
+--@Descripción: Trigger para actualizar la lista de propietarios
+
+set serveroutput on
+
+create or replace trigger tr_historial_propietarios
+	before insert or update of propietario_id on vehiculo
+	for each row
+declare
+	v_historico_propietario_id historico_propietario.historico_propietario_id%type;
+
+begin
+	case 
+		when updating then
+			-- Si el propietario es el mismo
+			if :old.propietario_id = :new.propietario_id then
+				raise_application_error(-20010, 'El cliente es el mismo');
+			end if;
+
+			-- Obteniendo el registro del histórico para completar el periodo
+			select historico_propietario_id into v_historico_propietario_id
+			from historico_propietario
+			where propietario_id = :old.propietario_id
+			and vehiculo_id = :old.vehiculo_id
+			and inicio_periodo = :old.inicio_periodo;
+
+			-- Se actualiza el registro del historial de propietarios
+			update historico_propietario
+			set fin_periodo = sysdate
+			where historico_propietario_id = v_historico_propietario_id;
+	end case;
+	
+	-- Insertando en historico propietario
+	insert into historico_propietario(historico_propietario_id, inicio_periodo,
+		propietario_id, vehiculo_id)
+	values(seq_historico_propietario.nextval, :new.inicio_periodo,
+		:new.propietario_id, :new.vehiculo_id);
+end;
