e395dc67428b07df1d7b50fe1192f44fc91594a5
Antonio <antoniotorr5555@gmail.com>
2020-06-19 15:51:35 -0500

    Carga inicial placas, modelo, marca y entidad

---
+++ Scripts/s-09-carga-marca.sql
@@ -0,0 +1,12 @@
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'MITSUBISHI', 'Es una empresa que fabrica automóviles');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'DODGE', 'Es una marca de automóviles y camiones');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'AUDI', 'Es una empresa que fabrica automóviles');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'CADILLAC', 'Es una empresa que fabrica automóviles');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'CHEVROLET', 'Es una empresa multinacional especializada en la industria automotriz');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'VOLKSWAGEN', 'Es una empresa multinacional especializada en la industria automotriz');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'BMW', 'Es una marca de automóviles y camiones');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'MAZDA', 'Es una marca de automóviles y camiones');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'MERCEDES-BENZ', 'Es una empresa que fabrica automóviles');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'ACURA', 'Es una empresa que fabrica automóviles');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'FORD', 'Es una marca de automóviles y camiones');
+insert into marca (marca_id, clave, descripcion) values (seq_marca.nextval, 'GMC', 'Es una marca de automóviles y camiones');
