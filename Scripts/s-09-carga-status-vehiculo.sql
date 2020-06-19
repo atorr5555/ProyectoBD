a8f285fc78cdf0c11c9976964a25c966b18f4c35
Antonio <antoniotorr5555@gmail.com>
2020-06-19 16:24:31 -0500

    Avance en la carga inicial

---
+++ Scripts/s-09-carga-status-vehiculo.sql
@@ -0,0 +1,4 @@
+insert into status_vehiculo(status_vehiculo_id, clave, descripcion) values(seq_status_vehiculo.nextval, 'EN REGLA', 'VEHICULO EN REGLA');
+insert into status_vehiculo(status_vehiculo_id, clave, descripcion) values(seq_status_vehiculo.nextval, 'CON LICENCIA EXPIRADA', 'PROPIETARIO CON LICENCIA EXPIRADA');
+insert into status_vehiculo(status_vehiculo_id, clave, descripcion) values(seq_status_vehiculo.nextval, 'CON ADEUDO DE IMPUESTO', 'NO HA PAGADO ALGUNA CUOTA ANUAL');
+insert into status_vehiculo(status_vehiculo_id, clave, descripcion) values(seq_status_vehiculo.nextval, 'CON VERIFICACION PENDIENTE', 'DEBE IR A UN CENTRO DE VERIFICACION');
