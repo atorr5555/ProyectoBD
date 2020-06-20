e277150a4882ccb04aa3674f006ed4e3fd49b5e3
Antonio <antoniotorr5555@gmail.com>
2020-06-19 20:20:35 -0500

    Invocación de scripts

--- Scripts/s-09-carga-inicial.sql
+++ Scripts/s-09-carga-inicial.sql
@@ -2,9 +2,16 @@
 --@Fecha creación: 19/06/2020
 --@Descripción: Carga inicial de datos
 
-whenever sqlerror rollback;
+whenever sqlerror exit;
 
-trunc(dbms_random.value(1, v_count))
-trunc(dbms_random.value(1, v_count))
+@s-09-carga-entidad.sql
+@s-09-carga-placa.sql
+@s-09-carga-marca.sql
+@s-09-carga-modelo.sql
+@s-09-carga-propietario.sql
+@s-09-carga-status-vehiculo.sql
+@s-09-carga-vehiculo.sql
+@s-09-carga-tipo-licencia.sql
+@s-09-carga-licencia.sql
 
 whenever sqlerror continue;
