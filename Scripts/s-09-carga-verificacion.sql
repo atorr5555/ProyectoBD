aab99e64bae628cf1046e974074de50ee3613e85
Antonio <antoniotorr5555@gmail.com>
2020-06-20 19:52:53 -0500

    Carga verificacion

---
+++ Scripts/s-09-carga-verificacion.sql
@@ -0,0 +1,36 @@
+--@Autor: FLores Fuentes Kevin y Torres Verastegui Jose Antonio
+--@Fecha creación: 20/06/2020
+--@Descripción: Carga de verificaciones, una por cada notificación
+
+declare
+	cursor cur_notificacion is
+		select registro_mediciones_id, fecha
+		from notificacion;
+	v_vehiculo_id number;
+	v_verificacion_id number;
+	v_fecha date;
+begin
+	for r in cur_notificacion loop
+		select vehiculo_id into v_vehiculo_id
+		from registro_mediciones
+		where registro_mediciones_id = r.registro_mediciones_id;
+
+		select seq_verificacion.nextval into v_verificacion_id
+		from dual;
+
+		v_fecha := r.fecha + trunc(dbms_random.value(1,5));
+
+		insert into verificacion(verificacion_id, fecha_verificacion,
+			folio_verificacion, vehiculo_id)
+		values(v_verificacion_id, v_fecha, v_verificacion_id||v_vehiculo_id,
+			v_vehiculo_id);
+
+		-- Por cada verificacion también se inserta una nueva medición
+		insert into registro_mediciones(registro_mediciones_id, hc, co, nox, co2,
+			fecha, vehiculo_id)
+		values(seq_registro_mediciones.nextval, trunc(dbms_random.value(30, 100)), 
+			trunc(dbms_random.value(0,0.05), 3), trunc(dbms_random.value(0,3), 3),
+			trunc(dbms_random.value(), 3), v_fecha,  v_vehiculo_id);
+	end loop;
+end;
+/
