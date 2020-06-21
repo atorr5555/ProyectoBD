13581c3012bdef501d24e2442586a81c8560607e
Antonio <antoniotorr5555@gmail.com>
2020-06-21 15:36:39 -0500

    índices de uso frecuente en joins

--- Scripts/s-06-índices.sql
+++ Scripts/s-06-índices.sql
@@ -9,18 +9,21 @@ create index email_ix on propietario(email);
 create index importe_ix on pago_cuota(importe);
 create index RFC_ix on propietario(RFC);
 create index clave_ix on entidad(clave);
+create index particular_id_ix on particular(vehiculo_id);
+create index carga_id_ix on carga(vehiculo_id);
+create index transporte_publico_id_ix on transporte_publico(vehiculo_id);
+create index biometria_id_ix on biometria(licencia_id);
+create index registro_mediciones_fk_ix on notificacion(registro_mediciones_id);
+create index vehiculo_fk_ix on registro_mediciones(vehiculo_id);
 
 Prompt creando indices unique:
---create unique index nombre_iuk on  propietario(nombre);
---create unique index fecha_status_iuk on vehiculo(fecha_status);
-create unique index clave_Status_iuk on status_vehiculo(clave);
+create unique index clave_status_iuk on status_vehiculo(clave);
 create unique index num_licencia_iuk on licencia(num_licencia);
 
 
 Prompt creando indices compuestos:
 create unique index vehiculo_propietario_iuk on vehiculo(vehiculo_id,propietario_id);
 create unique index vehiculo_placa_iuk on vehiculo(vehiculo_id,placa_id);
---create unique index folio_importe_iuk on pago_cuota(folio,importe);
 create unique index propietario_licencia_iuk on licencia(propietario_id,licencia_id);
 
 Prompt creando indices a base de funciones:
